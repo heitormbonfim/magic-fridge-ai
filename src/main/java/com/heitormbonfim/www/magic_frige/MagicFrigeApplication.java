930fd08342e908922f83003db2d000415c8e5aa0
Heitor M Bonfim <71136688+heitormbonfim@users.noreply.github.com>
2025-04-19 21:16:11 -0300

    chore: template

---
+++ src/main/java/com/heitormbonfim/www/magic_frige/MagicFrigeApplication.java
@@ -0,0 +1,13 @@
+package com.heitormbonfim.www.magic_frige;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MagicFrigeApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MagicFrigeApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/heitormbonfim/www/magic_frige/MagicFrigeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.heitormbonfim.www.magic_frige;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MagicFrigeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
