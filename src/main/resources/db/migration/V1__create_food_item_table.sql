9a5092400a2fa3e12092dfd5b9b55e8deb043947
Heitor M Bonfim <71136688+heitormbonfim@users.noreply.github.com>
2025-04-23 20:32:11 -0300

    feat: migration created

---
+++ src/main/resources/db/migration/V1__create_food_item_table.sql
@@ -0,0 +1,7 @@
+CREATE TABLE food_item (
+    id BIGINT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(255) NOT NULL,
+    category VARCHAR(255) NOT NULL,
+    amount INT NOT NULL,
+    dueDate DATE NOT NULL,
+);
