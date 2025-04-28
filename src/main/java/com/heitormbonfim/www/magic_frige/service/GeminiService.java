d328bda7a64572c5509ce83bc6c3239bde774c5e
heitormbonfim <heitor.mb27@gmail.com>
2025-04-27 21:58:43 -0300

    feat: completed GeminiService

--- src/main/java/com/heitormbonfim/www/magic_frige/service/GeminiService.java
+++ src/main/java/com/heitormbonfim/www/magic_frige/service/GeminiService.java
@@ -1,7 +1,13 @@
 package com.heitormbonfim.www.magic_frige.service;
 
+import org.springframework.http.HttpHeaders;
+import org.springframework.http.MediaType;
 import org.springframework.stereotype.Service;
 import org.springframework.web.reactive.function.client.WebClient;
+import reactor.core.publisher.Mono;
+
+import java.util.List;
+import java.util.Map;
 
 @Service
 public class GeminiService {
@@ -11,5 +17,22 @@ public class GeminiService {
         this.webClient = webClient;
    }
 
-    public void generateRecipe() {}
+    public Mono<String> generateRecipe() {
+        String prompt = "Qual a origem da palavra cálculo?";
+        Map<String, Object> requestBody = Map.of(
+                "contents", List.of(
+                        Map.of(
+                                "parts", List.of(
+                                        Map.of("text", prompt)
+                                )
+                        )
+                )
+        );
+
+        return webClient.post()
+                .header(HttpHeaders.CONTENT_TYPE, MediaType.APPLICATION_JSON_VALUE)
+                .bodyValue(requestBody)
+                .retrieve()
+                .bodyToMono(String.class);
+    }
 }
